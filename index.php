79722f59a64036931a7edfbdb3ce2ffb828bf54d
Ermanno Lo Cascio <ermannolocascio@gmail.com>
2024-04-02 15:52:36 +0200

    horizontal scrolling for table added

--- index.php
+++ index.php
@@ -38,57 +38,58 @@ include_once 'includes/header.php';
                                 DataTable
                             </div>
                             <div class="card-body">
-                            
-                            <?php 
-                                // Check if the session variable is set and not empty
-                                if (isset($_SESSION['Columns']) && !empty($_SESSION['Columns'])) {
-                                    // Check if $_SESSION['Columns'] is an array
-                                    if (is_array($_SESSION['Columns'])) {
-                                        // If it's an array, you need to loop through it to display each element
-                                        echo "<table id='tableData' class='display' style='width:100%'>";
+                                <div style="overflow-x: auto;">
+                                    <?php 
+                                    // Check if the session variable is set and not empty
+                                    if (isset($_SESSION['Columns']) && !empty($_SESSION['Columns'])) {
+                                        // Check if $_SESSION['Columns'] is an array
+                                        if (is_array($_SESSION['Columns'])) {
+                                            // If it's an array, you need to loop through it to display each element
+                                            echo "<table id='tableData' class='display' style='min-width: 100%'>";
 
-                                        // Must re-initilize the table to retreive jQuery features from https://datatables.net/
-                                        echo "<script> $(document).ready(function() {
-                                            $('#tableData').DataTable();
-                                            });
-                                          </script>";  
+                                            // Must re-initilize the table to retrieve jQuery features from https://datatables.net/
+                                            echo "<script> $(document).ready(function() {
+                                                $('#tableData').DataTable();
+                                                });
+                                            </script>";  
 
 
-                                        echo "<thead>"; 
-                                        echo "<tr>";
-                                        foreach ($_SESSION['Columns'] as $column) {
-                                            // Extract the 'title' property from the object
-                                            $title = isset($column['title']) ? $column['title'] : '';
-                                            if (!empty($title)) {
-                                                echo "<th>$title</th>";
-                                            }
-                                        }
-                                        echo "</tr>";
-                                        echo "</thead>";
-
-                                        // Build Table body
-                                        
-                                        echo "<tbody>";
-                                        
-                                        // Loop through each row to populate the table body
-                                        foreach ($_SESSION['Rows'] as $row) {
+                                            echo "<thead>"; 
                                             echo "<tr>";
-                                            // Loop through each column value in the row
-                                            foreach ($row as $columnValue) {
-                                                echo "<td>$columnValue</td>";
+                                            foreach ($_SESSION['Columns'] as $column) {
+                                                // Extract the 'title' property from the object
+                                                $title = isset($column['title']) ? $column['title'] : '';
+                                                if (!empty($title)) {
+                                                    echo "<th>$title</th>";
+                                                }
                                             }
                                             echo "</tr>";
+                                            echo "</thead>";
+
+                                            // Build Table body
+
+                                            echo "<tbody>";
+
+                                            // Loop through each row to populate the table body
+                                            foreach ($_SESSION['Rows'] as $row) {
+                                                echo "<tr>";
+                                                // Loop through each column value in the row
+                                                foreach ($row as $columnValue) {
+                                                    echo "<td>$columnValue</td>";
+                                                }
+                                                echo "</tr>";
+                                            }
+                                            echo "</tbody>";
+                                            echo "</table>";  
                                         }
-                                        echo "</tbody>";
-                                        echo "</table>";  
+                                    } else {
+                                        echo "<table id='tableData' class='display' style='width:100%'></table>";
+                                        // echo "<p>No table data available.</p>";
                                     }
-                                } else {
-                                    echo "<table id='tableData' class='display' style='width:100%'></table>";
-                                    // echo "<p>No table data available.</p>";
-                                }
-                                ?>
-                            
+                                    ?>
+                                </div>
                             </div>
+
                         </div>
                     </div>
                 </main>
