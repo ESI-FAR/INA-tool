51833f99e1a4045982055719c93ec12fbe05d292
Ermanno Lo Cascio <ermannolocascio@gmail.com>
2024-06-18 16:02:32 +0200

    update download filename

--- includes/downloader.js
+++ includes/downloader.js
@@ -69,11 +69,18 @@ function downloadProject() {
         const file = input.files[0];
         if (file) {
             // Generate timestamp
-            const timestamp = new Date().toISOString().replace(/[:.-]/g, '');
+            const now = new Date();
+            const year = now.getFullYear();
+            const month = String(now.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed
+            const day = String(now.getDate()).padStart(2, '0');
+            const hours = String(now.getHours()).padStart(2, '0');
+            const minutes = String(now.getMinutes()).padStart(2, '0');
+            const seconds = String(now.getSeconds()).padStart(2, '0');
+            const formattedDateTime = `${year}${month}${day}-${hours}${minutes}${seconds}`;
             // Add time stamp to the file name for the link
-            downloadLink.download =  timestamp + '_' + file.name + '.png';
+            downloadLink.download =  formattedDateTime + '_' + file.name.slice(0,-4) + '.png';
         } else {
-            downloadLink.download = "No file available";
+            alert("No image to download");
         }
 
         document.body.appendChild(downloadLink);
