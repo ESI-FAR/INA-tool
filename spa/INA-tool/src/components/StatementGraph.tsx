06a5e242b43abe5132df9716a6e2a86277e83780
sverhoeven <s.verhoeven@esciencecenter.nl>
2025-01-14 13:33:16 +0000

    Basic rete visualization

--- spa/INA-tool/src/components/StatementGraph.tsx
+++ spa/INA-tool/src/components/StatementGraph.tsx
@@ -1,7 +1,13 @@
+import { createEditor } from "@/lib/graph";
+import { useRete } from "rete-react-plugin";
+
 export function StatementGraph() {
+  const [ref, editor] = useRete(createEditor)
+
   return (
     <div>
       <h1>GraphCanvas</h1>
+      <div ref={ref} className="w-screen h-96"></div>
     </div>
   );
 }
