3329d4fef0c32b39330326fc13adca186aaca6bf
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2025-01-22 15:31:01 +0100

    Define schema for connection + fix casing

--- spa/INA-tool/src/lib/schema.ts
+++ spa/INA-tool/src/lib/schema.ts
@@ -7,15 +7,38 @@ const statementSchema = z.object({
   Deontic: z.string().optional(),
   Aim: z.string(),
   "Direct Object": z.string().optional(),
-  "Type Of Direct Object": z.string().optional(),
+  "Type of Direct Object": z.string().optional(),
   "Indirect Object": z.string().optional(),
-  "Type Of Indirect Object": z.string().optional(),
+  "Type of Indirect Object": z.string().optional(),
   "Activation Condition": z.string().optional(),
   "Execution Constraint": z.string().optional(),
   "Or Else": z.string().optional(),
 });
 export type Statement = z.infer<typeof statementSchema>;
-export const statementColumns = Object.keys(statementSchema.shape);
+export const statementColumns = Object.keys(
+  statementSchema.shape,
+) as ReadonlyArray<keyof Statement>;
 
 export const statementsSchema = z.array(statementSchema);
 export type Statements = z.infer<typeof statementsSchema>;
+
+const connectionSchema = z.object({
+  source_statement: z.string(),
+  source_node: z.union([
+    z.literal("direct-object"),
+    z.literal("indirect-object"),
+    z.literal("aim"),
+  ]),
+  target_statement: z.string(),
+  target_node: z.union([
+    z.literal("attribute"),
+    z.literal("activation-condition"),
+  ]),
+  driver: z.union([
+    z.literal("actor"),
+    z.literal("outcome"),
+    z.literal("sanction"),
+  ]),
+});
+export type Connection = z.infer<typeof connectionSchema>;
+export const connectionColumns = Object.keys(connectionSchema.shape) as ReadonlyArray<keyof Connection>;;
