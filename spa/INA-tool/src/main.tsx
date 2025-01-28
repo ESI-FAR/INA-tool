fc223f0eb2ee52a7ea3d63d7756f1bbdc2bbe6d9
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2025-01-28 15:07:47 +0100

    Use hash routing
    As GitHub pages does not suppoert rewrites to index.html

--- spa/INA-tool/src/main.tsx
+++ spa/INA-tool/src/main.tsx
@@ -1,13 +1,19 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
+import {
+  RouterProvider,
+  createHashHistory,
+  createRouter,
+} from "@tanstack/react-router";
 import "./index.css";
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
 
+const hashHistory = createHashHistory();
+
 // Create a new router instance
-const router = createRouter({ routeTree });
+const router = createRouter({ routeTree, history: hashHistory });
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
