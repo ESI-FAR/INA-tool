274fca5d2859f029a6b18d670c1f44e082187515
sverhoeven <s.verhoeven@esciencecenter.nl>
2025-02-04 09:48:20 +0100

    Sync privacy policy with currently used technology

--- spa/INA-tool/src/routes/privacy-policy.tsx
+++ spa/INA-tool/src/routes/privacy-policy.tsx
@@ -6,59 +6,69 @@ export const Route = createFileRoute("/privacy-policy")({
 
 function RouteComponent() {
   return (
-    <div className="container-fluid px-4">
-      <h1 className="mt-4">Privacy Policy</h1>
-      <ol className="breadcrumb mb-4">
-        <li className="breadcrumb-item">
-          <a href="index.php">Dashboard</a>
-        </li>
-        <li className="breadcrumb-item active">Privacy Policy</li>
-      </ol>
-      <div className="card mb-4">
+    <div className="container mx-auto px-4">
+      <h1 className="mt-4 text-4xl font-bold">Privacy Policy</h1>
+      <div className="">
         <div className="card-body">
-          <h1>Privacy Policy</h1>
-
-          <h2>Overview</h2>
-          <ul>
-            This web application uses a PHP session cookie to enhance your
-            browsing experience. This cookie is essential for the operation of
-            the site and is not used for tracking purposes.
+          <h2 className="mb-2 text-2xl font-semibold">Overview</h2>
+          <ul className="mb-4 list-inside list-disc">
+            <li>This web application uses your web browsers storage.</li>
           </ul>
 
-          <h2>Data Collection and Storage</h2>
-          <ul>
+          <h2 className="mb-2 text-2xl font-semibold">
+            Data Collection and Storage
+          </h2>
+          <ul className="mb-4 list-inside list-disc">
             <li>We do not collect any personal data from users.</li>
             <li>
-              No data entered or generated during your session is stored in any
-              database on our servers.
+              All data entered or generated during your session is stored in
+              your web browser.
+            </li>
+          </ul>
+
+          <h2 className="mb-2 text-2xl font-semibold">Third-Party Services</h2>
+          <ul className="mb-4 list-inside list-disc">
+            <li>
+              This web application is hosted at{" "}
+              <a
+                target="_blank"
+                className="underline"
+                href="https://pages.github.com/"
+              >
+                Github Pages
+              </a>
+              , so GitHub can see who visits.
             </li>
             <li>
-              All information is stored locally on your device and is lost as
-              soon as your session is terminated by the user.
+              Our website does not utilize any third-party services that track
+              users or collect personal information.
             </li>
-            <li>Each session last 1440 seconds.</li>
           </ul>
 
-          <h2>Third-Party Services</h2>
-          <ul>
-            Our website does not utilize any third-party services that track
-            users or collect personal information.
+          <h2 className="mb-2 text-2xl font-semibold">
+            Changes to This Policy
+          </h2>
+          <ul className="mb-4 list-inside list-disc">
+            <li>
+              We may update our Privacy Policy from time to time. Any changes
+              will be reflected on this page.
+            </li>
           </ul>
 
-          <h2>Changes to This Policy</h2>
-          <ul>
-            We may update our Privacy Policy from time to time. Any changes will
-            be reflected on this page.
-          </ul>
-
-          <h2>Contact Us</h2>
-          <ul>
-            If you have any questions about this Privacy Policy, please contact
-            the software contributors via the{" "}
-            <a href="https://github.com/ESI-FAR/INA-tool" target="blank">
-              GitHub
-            </a>{" "}
-            page.
+          <h2 className="mb-2 text-2xl font-semibold">Contact Us</h2>
+          <ul className="mb-4 list-inside list-disc">
+            <li>
+              If you have any questions about this Privacy Policy, please
+              contact the software contributors via the{" "}
+              <a
+                href="https://github.com/ESI-FAR/INA-tool"
+                target="_blank"
+                className="underline"
+              >
+                GitHub
+              </a>{" "}
+              page.
+            </li>
           </ul>
         </div>
       </div>
