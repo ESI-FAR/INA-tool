abb1abca6ed56e892c43e3cbeadde5fc2bf16d8e
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2025-02-19 14:05:09 +0100

    WIP: Trying to specify ports for elkjs to get similar layout as initial
    Nodes are more staircased, but not good enough

--- src/components/LayoutButton.tsx
+++ src/components/LayoutButton.tsx
@@ -4,7 +4,7 @@ import ELK from "elkjs/lib/elk-api";
 import ELKworker from "elkjs/lib/elk-worker.js?worker";
 import { LayoutTemplateIcon } from "lucide-react";
 import { Button } from "./ui/button";
-import { isStatementNode } from "@/lib/node";
+import { isComponentNode, isStatementNode } from "@/lib/node";
 import type { INANode, StatementNode } from "@/lib/node";
 import { INASEdge, INACEdge } from "@/lib/edge";
 
@@ -18,10 +18,66 @@ const elk = new ELK({
 // see https://reactflow.dev/examples/layout/elkjs-multiple-handles
 const layoutOptions = {
   "elk.algorithm": "layered",
-  "elk.layered.spacing.nodeNodeBetweenLayers": 100,
-  "elk.spacing.nodeNode": 40,
-  "elk.direction": "RIGHT",
+  "elk.layered.spacing.nodeNodeBetweenLayers": 140,
+  'elk.layered.nodePlacement.strategy': 'SIMPLE',
+  "elk.spacing.nodeNode": 240,
 } as const;
+
+type Port = {
+  id: string;
+  properties?: {
+    side?: "LEFT" | "RIGHT" | "TOP" | "BOTTOM";
+    'port.index'?: number;
+  };
+};
+
+function computePorts(node: INANode) {
+  const ports: Port[] = [{ id: node.id }];
+  if (!isComponentNode(node)) {
+    return ports;
+  }
+  switch (node.type) {
+    case "Activation Condition":
+      return ports.concat([
+        { id: node.parentId + '-activation-condition-2-attribute', properties: { side: "RIGHT" } },
+        { id: "outcome", properties: { side: "TOP", 'port.index': 0 } },
+        { id: "sanction", properties: { side: "TOP", 'port.index': 1 } },
+      ]);
+    case "Attribute":
+      return ports.concat([
+        { id: node.parentId + "-activation-condition-2-attribute", properties: { side: "RIGHT" } },
+        { id: node.parentId + '-attribute-2-aim', properties: { side: "LEFT" } },
+        { id: "actor", properties: { side: "TOP" } },
+      ]);
+    case "Aim":
+      return ports.concat([
+        { id: node.parentId + '-attribute-2-aim', properties: { side: "LEFT" } },
+        { id: node.parentId + "-aim-2-direct-object", properties: { side: "RIGHT" } },
+        { id: node.parentId + '-aim-2-execution-constraint', properties: { side: "BOTTOM", 'port.index': 0 } },
+        { id: "sanction", properties: { side: "BOTTOM", 'port.index': 1 } },
+      ]);
+    case "Direct Object":
+      return ports.concat([
+        { id: node.parentId + '-aim-2-direct-object', properties: { side: "LEFT" } },
+        { id: node.parentId + '-direct-object-2-indirect-object', properties: { side: "BOTTOM", 'port.index': 0 } },
+        { id: "actor", properties: { side: "BOTTOM", 'port.index': 1 } },
+        { id: "outcome", properties: { side: "BOTTOM", 'port.index': 1 } },
+      ]);
+    case "Indirect Object":
+      return ports.concat([
+        { id: node.parentId + '-direct-object-2-indirect-object', properties: { side: "TOP" } },
+        { id: "actor", properties: { side: "BOTTOM" } },
+        { id: "outcome", properties: { side: "BOTTOM" } },
+      ]);
+    case "Execution Constraint":
+      return ports.concat([
+        { id: node.parentId + '-aim-2-execution-constraint', properties: { side: "TOP" } },
+        { id: "actor", properties: { side: "BOTTOM" } },
+      ]);
+    default:
+      throw new Error(`Unknown component type ${node.type}`);
+  }
+}
 
 const useComponentLayout = () => {
   const { getNodes, setNodes, getEdges, fitView } = useReactFlow<
@@ -39,19 +95,29 @@ const useComponentLayout = () => {
         "elk.direction": "RIGHT",
       },
       children: statementNodes.map((node) => {
+        const ports = [{ id: node.id }];
         return {
           ...node,
           children: getNodes()
             .filter((n) => n.parentId === node.id && !n.hidden)
             .map((component) => {
+              const cports = computePorts(component);
               return {
                 ...component,
                 width: component.measured!.width,
                 height: component.measured!.height,
+                // properties: {
+                //   "org.eclipse.elk.portConstraints": "FIXED_ORDER",
+                // },
+                // ports: cports,
               };
             }),
           width: node.measured!.width,
           height: node.measured!.height,
+          // properties: {
+          //   "org.eclipse.elk.portConstraints": "FIXED_ORDER",
+          // },
+          // ports,
         };
       }),
       edges,
