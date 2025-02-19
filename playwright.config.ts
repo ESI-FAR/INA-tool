b21a1d7ce9e8ee8a9f04d49e9ba1ecdcab76b2d2
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2025-02-19 15:12:45 +0100

    Replace 127.0.0.1 with localhost

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://127.0.0.1:5173",
+    baseURL: "http://localhost:5173",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -75,6 +75,5 @@ export default defineConfig({
     command: "npm run dev",
     port: 5173,
     reuseExistingServer: !process.env.CI,
-    stdout: "pipe",
   },
 });
