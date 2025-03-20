0d8a95d869fbb5279c6d9299c537757b60b6a4e6
sverhoeven <s.verhoeven@esciencecenter.nl>
2025-03-20 13:58:48 +0100

    Make screenshot also for SVG + screenshot of what you see
    No longer whole network

--- src/components/ScreenshotButton.tsx
+++ src/components/ScreenshotButton.tsx
@@ -1,59 +1,62 @@
 import { CameraIcon } from "lucide-react";
-import { useCallback } from "react";
-import {
-  getNodesBounds,
-  getViewportForBounds,
-  useReactFlow,
-} from "@xyflow/react";
-import { toPng } from "html-to-image";
+import { toPng, toSvg } from "html-to-image";
 import { store } from "@/stores/global";
-import { DropdownMenuItem } from "./ui/dropdown-menu";
+import {
+  DropdownMenuItem,
+  DropdownMenuPortal,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+} from "./ui/dropdown-menu";
 
-function downloadImage(dataUrl: string) {
+function downloadImage(dataUrl: string, extension: string) {
   const projectName = store.getState().projectName;
   const a = document.createElement("a");
 
-  a.setAttribute("download", `${projectName}.png`);
+  a.setAttribute("download", `${projectName}.${extension}`);
   a.setAttribute("href", dataUrl);
   a.click();
   a.remove();
 }
 
-const imageWidth = 1920;
-const imageHeight = 1080;
+function getViewportElement() {
+  const element = document.querySelector(
+    ".react-flow__viewport",
+  ) as HTMLElement | null;
+  if (!element) {
+    throw new Error("Element not found");
+  }
+  return element;
+}
+
+async function exportToSvg() {
+  const element = getViewportElement();
+  const value = await toSvg(element);
+  downloadImage(value, "svg");
+}
+
+async function exportToPng() {
+  const rootClasses = window.document.documentElement.classList;
+  const backgroundColor = rootClasses.contains("dark") ? "black" : "white";
+  const element = getViewportElement();
+  const value = await toPng(element, {
+    backgroundColor,
+  });
+  downloadImage(value, "png");
+}
 
 export function ScreenshotButton() {
-  const { getNodes } = useReactFlow();
-  const onClick = useCallback(() => {
-    const nodesBounds = getNodesBounds(getNodes());
-    const viewport = getViewportForBounds(
-      nodesBounds,
-      imageWidth,
-      imageHeight,
-      0.5,
-      2,
-      0,
-    );
-    const element = document.querySelector(
-      ".react-flow__viewport",
-    ) as HTMLElement | null;
-    if (!element) {
-      console.error("Element not found");
-      return;
-    }
-    toPng(element, {
-      backgroundColor: "white",
-      width: imageWidth,
-      height: imageHeight,
-      style: {
-        transform: `translate(${viewport.x}px, ${viewport.y}px) scale(${viewport.zoom})`,
-      },
-    }).then(downloadImage);
-  }, [getNodes]);
-
   return (
-    <DropdownMenuItem onClick={onClick} title="Take a screenshot">
-      <CameraIcon /> Screenshot
-    </DropdownMenuItem>
+    <DropdownMenuSub>
+      <DropdownMenuSubTrigger title="Take a screenshot">
+        <CameraIcon /> Screenshot
+      </DropdownMenuSubTrigger>
+      <DropdownMenuPortal>
+        <DropdownMenuSubContent>
+          <DropdownMenuItem onClick={exportToPng}>PNG</DropdownMenuItem>
+          <DropdownMenuItem onClick={exportToSvg}>SVG</DropdownMenuItem>
+        </DropdownMenuSubContent>
+      </DropdownMenuPortal>
+    </DropdownMenuSub>
   );
 }
